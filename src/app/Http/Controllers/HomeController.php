e3085e931042621157e74298775680080984a7df
fukuba-shuma <calculus.fukuba@gmail.com>
2023-01-27 22:19:18 +0900

    modelにsqlを切り出し

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -5,16 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Language;
 use App\Models\LearningContent;
 use App\Models\LearningTime;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
         $user_id = $request->user_id;
-        return self::read_learning_log($user_id);
+        return LearningTime::read_learning_log($user_id);
     }
 
     public function read_langs_contents(Request $request)
@@ -51,63 +49,7 @@ class HomeController extends Controller
             $learning_time->learning_contents()->attach($content);
         }
 
-        return  self::read_learning_log($user_id);
+        return  LearningTime::read_learning_log($user_id);
     }
 
-    private function read_learning_log(string $user_id)
-    { // 今日の学習時間
-        $today = Carbon::today();
-        $today_time = LearningTime::whereDate('learning_time_date', $today)
-            ->where('user_id', '=', $user_id)
-            ->sum('learning_time');
-
-
-        $month = Carbon::today()->subDay(30);
-        $monthly_time_query = LearningTime::whereDate('learning_time_date', '>=', $month)
-            ->where('user_id', '=', $user_id);
-
-        // 今月の学習時間
-        $monthly_sum = $monthly_time_query->sum('learning_time');
-
-        // 棒グラフ用のデータ
-        $monthly_learning_times = $monthly_time_query
-            ->orderBy('learning_time_date', 'asc')
-            ->get(['learning_time', 'learning_time_date']);
-
-        // 累計学習時間
-        $total_sum = LearningTime::where('user_id', '=', $user_id)->sum('learning_time');
-
-        // PieChart
-        $content_circle = DB::table('learning_contents')
-            ->join('learning_content_learning_time', 'learning_contents.learning_content_id', '=', 'learning_content_learning_time.learning_content_id')
-            ->join('learning_times', 'learning_times.learning_time_id', '=', 'learning_content_learning_time.learning_time_id')
-            ->selectRaw('learning_contents.name as name')
-            ->selectRaw('COUNT(learning_contents.name) as ratio')
-            ->whereDate('learning_times.learning_time_date', '>=', $month)
-            ->where('learning_times.user_id', '=', $user_id)
-            ->groupBy('learning_contents.name')
-            ->get();
-
-        $language_circle = DB::table('languages')
-            ->join('language_learning_time', 'languages.language_id', '=', 'language_learning_time.language_id')
-            ->join('learning_times', 'learning_times.learning_time_id', '=', 'language_learning_time.learning_time_id')
-            ->selectRaw('languages.language as name')
-            ->selectRaw('COUNT(languages.language) as ratio')
-            ->whereDate('learning_times.learning_time_date', '>=', $month)
-            ->where('learning_times.user_id', '=', $user_id)
-            ->groupBy('languages.language')
-            ->get();
-
-        return json_encode(
-            compact(
-                'today_time',
-                'monthly_sum',
-                'total_sum',
-                'monthly_learning_times',
-                'content_circle',
-                'language_circle'
-            ),
-            JSON_UNESCAPED_UNICODE
-        );
-    }
 }
--- src/app/Models/LearningTime.php
+++ src/app/Models/LearningTime.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class LearningTime extends Model
 {
@@ -25,5 +27,63 @@ class LearningTime extends Model
         return $this->belongsToMany(
             Language::class,
         )->withTimestamps();
+    }
+
+
+    public static function read_learning_log(string $user_id)
+    { // 今日の学習時間
+        $today = Carbon::today();
+        $today_time = LearningTime::whereDate('learning_time_date', $today)
+            ->where('user_id', '=', $user_id)
+            ->sum('learning_time');
+
+
+        $month = Carbon::today()->subDay(30);
+        $monthly_time_query = LearningTime::whereDate('learning_time_date', '>=', $month)
+            ->where('user_id', '=', $user_id);
+
+        // 今月の学習時間
+        $monthly_sum = $monthly_time_query->sum('learning_time');
+
+        // 棒グラフ用のデータ
+        $monthly_learning_times = $monthly_time_query
+            ->orderBy('learning_time_date', 'asc')
+            ->get(['learning_time', 'learning_time_date']);
+
+        // 累計学習時間
+        $total_sum = LearningTime::where('user_id', '=', $user_id)->sum('learning_time');
+
+        // PieChart
+        $content_circle = DB::table('learning_contents')
+            ->join('learning_content_learning_time', 'learning_contents.learning_content_id', '=', 'learning_content_learning_time.learning_content_id')
+            ->join('learning_times', 'learning_times.learning_time_id', '=', 'learning_content_learning_time.learning_time_id')
+            ->selectRaw('learning_contents.name as name')
+            ->selectRaw('COUNT(learning_contents.name) as ratio')
+            ->whereDate('learning_times.learning_time_date', '>=', $month)
+            ->where('learning_times.user_id', '=', $user_id)
+            ->groupBy('learning_contents.name')
+            ->get();
+
+        $language_circle = DB::table('languages')
+            ->join('language_learning_time', 'languages.language_id', '=', 'language_learning_time.language_id')
+            ->join('learning_times', 'learning_times.learning_time_id', '=', 'language_learning_time.learning_time_id')
+            ->selectRaw('languages.language as name')
+            ->selectRaw('COUNT(languages.language) as ratio')
+            ->whereDate('learning_times.learning_time_date', '>=', $month)
+            ->where('learning_times.user_id', '=', $user_id)
+            ->groupBy('languages.language')
+            ->get();
+
+        return json_encode(
+            compact(
+                'today_time',
+                'monthly_sum',
+                'total_sum',
+                'monthly_learning_times',
+                'content_circle',
+                'language_circle'
+            ),
+            JSON_UNESCAPED_UNICODE
+        );
     }
 }
